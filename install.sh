c26ffaf7877ba628608a16a32e724665baae4943
Bolukan <github@bolukan.nl>
2018-03-25 20:47:53 +0200

    Bootstrap installation

--- install.sh
+++ install.sh
@@ -1,19 +1,48 @@
 #!/bin/sh
-
-echo "Installation"
-echo "============"
 BASEDIR="https://raw.githubusercontent.com/Bolukan/raspbian-install/master"
-echo "To download and start this script:"
-echo "$ cd ~"
-echo "$ wget {$BASEDIR}/install.sh"
-echo "$ chmod +x install.sh"
-echo ""
 
-echo "General update"
-sudo apt-get update
-sudo apt-get upgrade
+introduction() {
+  echo ""
+  echo "Installation"
+  echo "============"
+  echo "To download and start this script:"
+  echo "$ cd ~"
+  echo "$ wget $BASEDIR/install.sh"
+  echo "$ chmod +x install.sh"
+  echo ""
+}
 
-echo "Install nginx php-fpm sqlite3"
-sudo apt-get install nginx php-fpm sqlite3
-wget {$BASEDIR}/var/www/html/index.php /var/www/html/.
-wget {$BASEDIR}/etc/nginx/sites-available/default /etc/nginx/sites-available/.
+general_update() {
+  echo ""
+  echo "General update"
+  echo "=============="
+  sudo apt-get update
+  sudo apt-get upgrade
+}
+
+install_webserver() {
+  echo ""
+  echo "Install nginx php-fpm sqlite3"
+  echo "============================="
+  # sudo apt-get install nginx php-fpm sqlite3
+
+  cd /var/www/html
+  sudo wget -N -nv $BASEDIR/var/www/html/index.php
+  mkdir /var/www/html/css
+  cd /var/www/html/css
+  sudo wget -N -nv https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css
+  mkdir /var/www/html/js
+  cd /var/www/html/js
+  sudo wget -N -nv https://code.jquery.com/jquery-3.2.1.slim.min.js
+  sudo wget -N -nv https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js
+  sudo wget -N -nv https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js
+
+  cd /etc/nginx/sites-available
+  sudo wget -N -nv $BASEDIR/etc/nginx/sites-available/default
+
+  sudo systemctl restart nginx
+}
+
+introduction
+# general_update
+install_webserver
